e26eadbf358371082d3b3972a625cf93b5c763a6
CPinoleo <96216906+CPinoleo@users.noreply.github.com>
2021-12-18 22:36:07 -0300

    cambio de algunos signos

--- tareaMinor2.py
+++ tareaMinor2.py
@@ -17,8 +17,8 @@ def F2(w,m):
 #sum(f2([0,0,0,0],k)**2 for k in range(10000+1))
 
 def gradF2(w,m):
-    F_1 = sum(4*f2(w,k)*(w[0]+k*w[1]/5+e**(k/5)) for k in range(1,m+1))
-    F_2 = sum(4*(k/5)*f2(w,k)*(w[0]+k*w[1]/5+e**(k/5)) for k in range(1,m+1))
+    F_1 = sum(4*f2(w,k)*(w[0]+k*w[1]/5-e**(k/5)) for k in range(1,m+1))
+    F_2 = sum(4*(k/5)*f2(w,k)*(w[0]+k*w[1]/5-e**(k/5)) for k in range(1,m+1))
     F_3 = sum(4*f2(w,k)*(w[2]+w[3]*np.sin(k/5)-np.cos(k/5)) for k in range(1,m+1))
     F_4 = sum(4*np.sin(k/5)*f2(w,k)*(w[2]+w[3]*np.sin(k/5)-np.cos(k/5)) for k in range(1,m+1))
     return [F_1,F_2,F_3,F_4]
